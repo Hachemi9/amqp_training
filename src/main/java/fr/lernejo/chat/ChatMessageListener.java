e98f2a6a2c19add2da49a30d816de7e8aed65e1f
Hachemi9 <atrounehachemi@gmail.com>
2021-12-07 18:44:47 +0100

    Get 10 Last Messages

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ChatMessageListener {
+    ChatMessageRepository repo = new ChatMessageRepository();
+
     public void onMessage(String message){
-        System.out.println(message);
+        repo.addChatMessage(message);
     }
 }
